b83a10b6da800f3658c59bc47331ab2032361fa0
aishwarya <aishwaryaj1608@gmail.com>
2026-06-12 13:01:12 +0530

    portfolio basic design done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule , MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'aishwaryajadhav';
-  isDarkMode = false;
+  isDarkMode = true;
 
   toggleTheme() {
     this.isDarkMode = !this.isDarkMode;
